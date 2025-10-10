e6f48eb3ea51162eb7aef2744077008af99d32a3
Yanmife Adegbola <yanmifeigwe@gmail.com>
2025-10-10 18:22:43 +0100

    fixed deployment issues

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -5,5 +5,6 @@ export default function AdminDashbboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <DashbboardLayout>{children}</DashbboardLayout>;
+  // return <DashbboardLayout>{children}</DashbboardLayout>;
+  return <div>{children}</div>;
 }
